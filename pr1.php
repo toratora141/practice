21929a7abdc1564e169a9de453b480d6ea7e55fe
nojima <torataro141@gmail.com>
2022-09-22 17:29:28 +0900

    feat

--- pr1.php
+++ pr1.php
@@ -1,5 +1,15 @@
 <?php 
 var_dump("init");
-var_dump("hello");
-var_dump("world");
 var_dump("");
+var_dump("");
+var_dump("");
+var_dump("");
+var_dump("");
+var_dump("");
+var_dump("");
+var_dump("");
+var_dump("aa");
+var_dump("aaa");
+var_dump("");
+var_dump("");
+
