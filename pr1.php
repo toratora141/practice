5d93d7b3d58b576a87969594620e8335d6e54a40
nojima <torataro141@gmail.com>
2022-12-14 15:34:21 +0900

    delete text

--- pr1.php
+++ pr1.php
@@ -1,14 +1,2 @@
 <?php 
 var_dump("init");
-var_dump("Good moning");
-var_dump("222");
-var_dump("2222");
-var_dump("333");
-var_dump("111");
-var_dump("1111");
-var_dump("111");
-var_dump("");
-var_dump("aa");
-var_dump("aaa");
-var_dump("333");
-var_dump("333");
